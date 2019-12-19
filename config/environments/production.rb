41cdc1b5fdaed337742a0703616129d04536ade4
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-12-19 10:56:09 -0700

    update production link

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,11 +33,7 @@ Rails.application.configure do
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-<<<<<<< HEAD
   config.action_controller.asset_host = 'dn2n0535iqo1t.cloudfront.net'
-=======
-  config.action_controller.asset_host = 'd2fkqy4gnwp0en.cloudfront.net'
->>>>>>> b824469ab4c1512e74ad81a68623fd45e78e2a5a
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
